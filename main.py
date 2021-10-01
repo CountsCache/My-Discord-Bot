7ab0159532640439624fbcd3b4ee1cbf99afebb3
Christopher Burwell <crburwell@yahoo.com>
2021-10-01 17:06:10 -0400

    adding wink command to bot!

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def get_quote():
   quote = json_data[0]['q'] + " -" + json_data[0]['a']
   return(quote)
 
+
+gets a random winking gif from the waifu api when a user uses the command $ink
+def get_waifu():
+  response = requests.get("https://api.waifu.pics/sfw/wink")
+  json_data = json.loads(response.text)
+  waifu = "*LOADING WINK...* \n " + json_data['url']
+  return(waifu)
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
@@ -25,16 +33,16 @@ async def on_message(message):
   if message.author == client.user:
     return
 
+    if message.content.startswith('$wink'):
+    waifu = get_waifu()
+    await message.channel.send(waifu)
+
   if message.content.startswith('$hello'):
     await message.channel.send('Hello!')
 
   if message.content.startswith('$inspire'):
     quote = get_quote()
     await message.channel.send(quote)
-
-  if message.content.startswith('$wink'):
-    waifu = get_waifu()
-    await message.channel.send(waifu)
 
 #Use TOKEN variable to run bot (keep secret)
 TOKEN = os.environ['key']
